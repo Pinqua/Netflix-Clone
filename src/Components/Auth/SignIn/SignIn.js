2fd28338971fe91966b7b9e90f69a38a6d675459
Piyush Sati <69719134+Pinqua@users.noreply.github.com>
2022-10-11 01:41:14 +0530

    Update SignIn.js

--- src/Components/Auth/SignIn/SignIn.js
+++ src/Components/Auth/SignIn/SignIn.js
@@ -114,8 +114,8 @@ function SignIn() {
             bot. Learn more.
           </p>
           {/*added just to indicate that sign in with google disable for the time being because of some issues.*/}
-          <p style={{color:"red"}}>   
-            Sign in with google disabled for the time being to remove the deceptive site ahead warning from the website. Try signing up/signing in using email.
+          <p style={{color:"white",fontSize:"0.9rem"}}>   
+            <span style={{color:"#e50914"}}>Note:</span> Sign in with google disabled for the time being to remove the deceptive site ahead warning from the website. Try signing up/signing in using email.
           </p>
         </div>
       </div>
